c84756123cc9300437c4f6eea2d9483f51d63be0
Igoy Nawamreh <igoynawamreh@gmail.com>
2018-02-03 15:32:00 +0700

    Updates

--- src/resources/assets/js/components/views/bantenprov/dd-sekolah/DDSekolahBar03.vue
+++ src/resources/assets/js/components/views/bantenprov/dd-sekolah/DDSekolahBar03.vue
@@ -1,59 +1,140 @@
 <template>
   <div style="width: 100%; height: 200px;">
-    <IEcharts :option="bar" :loading="loading"></IEcharts>
+    <IEcharts :option="bar" :loading="loading" :resizable="true"></IEcharts>
   </div>
 </template>
 
 <script>
-import IEcharts from 'vue-echarts-v3/src/full.js';
-
 export default {
-  components: {
-    IEcharts
-  },
   data () {
     return {
       loading: false,
       bar: {
         title: {
-          text: 'ECharts bar + Ajax',
+          show: true,
+          text: '',
           x: 'center',
-          textStyle:{
-            fontSize: 11
+          textStyle: {
+            fontSize: 16,
+            fontWeight: 'normal',
+            fontStyle: 'normal',
+            color: '#fff'
           }
         },
         tooltip: {
           show: true
         },
-        xAxis: {
-          data: [],
-          axisLabel :{
-            fontSize: 6
+        toolbox: {
+          show : true,
+          orient : 'horizontal',
+          bottom: 0,
+          x: 'center',
+          feature : {
+            dataView: {
+              show: true,
+              lang: ['Data view', 'Cancel', ''],
+              readOnly: true,
+              backgroundColor: 'rgba(0,0,0,.65)',
+              textareaColor: 'rgba(0,0,0,.5)',
+              textareaBorderColor: 'rgba(0,0,0,.5)',
+              textColor: '#fff',
+              buttonColor: '#ddd',
+              buttonTextColor: '#333'
+            },
+            saveAsImage: {
+              show: true,
+              backgroundColor: 'transparent',
+              excludeComponents: ['toolbox', 'visualMap']
+            }
           },
+          iconStyle: {
+            borderWidth: 1,
+            borderType: 'solid',
+            borderColor: '#fff'
+          }
+        },
+        xAxis: {
+          show: true,
+          data: [],
+          axisLabel: {
+            show: true,
+            fontSize: 8,
+            fontWeight: 'normal',
+            fontStyle: 'normal',
+            color: '#fff'
+          },
+          axisLine: {
+            show: true,
+            lineStyle: {
+              width: 1,
+              type: 'solid',
+              color: '#fff'
+            }
+          },
+          axisTick: {
+            show: true
+          },
+          splitArea: {
+            show: false
+          },
+          splitLine: {
+            show: false,
+            lineStyle: {
+              width: 1,
+              type: 'solid',
+              color: '#eee'
+            }
+          }
         },
         yAxis: {
-          axisLabel :{
-            fontSize: 6
+          show: false,
+          axisLabel: {
+            show: true,
+            fontSize: 8,
+            fontWeight: 'normal',
+            fontStyle: 'normal',
+            color: '#fff'
           },
+          axisLine: {
+            show: true,
+            lineStyle: {
+              width: 1,
+              type: 'solid',
+              color: '#fff'
+            }
+          },
+          axisTick: {
+            show: true
+          },
+          splitArea: {
+            show: false
+          },
+          splitLine: {
+            show: false,
+            lineStyle: {
+              width: 1,
+              type: 'solid',
+              color: '#eee'
+            }
+          }
         },
         series: [{
           type: 'bar',
           data: [],
           barWidth: 20,
-          barGap: '-100%'
+          barGap: '30%',
+          cursor: 'default',
+          itemStyle: {
+            color: '#CDDC39'
+          }
         }],
-        color: {
-          type: 'linear',
-          x: 0,
-          y: 0,
-          x2: 0,
-          y2: 1,
-          colorStops: [{
-              offset: 0, color: '#CDDC39' // color at 0% position
-          }, {
-              offset: 1, color: '#8BC34A' // color at 100% position
-          }],
-          globalCoord: false // false by default
+        label: {
+          show: true,
+          position: 'top',
+          fontSize: 8,
+          fontWeight: 'normal',
+          fontStyle: 'normal',
+          color: '#fff'
         }
       }
     }
@@ -66,22 +147,22 @@ export default {
       var res = response.data;
 
       /**
-      * response :
-      * console.log(res)
-      *
-      * xAxis
-      * console.log(res[0].xAxis.data)
-      * console.log(Object.values(res[0].xAxis.data))
-      *
-      * series data
-      * console.log(res[0].series[0].data)
-      *
-      * region
-      * console.log(res[0].xAxis.region)
-      *
-      * length
-      * console.log(res.length);
-      */
+       * response :
+       * console.log(res)
+       *
+       * xAxis
+       * console.log(res[0].xAxis.data)
+       * console.log(Object.values(res[0].xAxis.data))
+       *
+       * series data
+       * console.log(res[0].series[0].data)
+       *
+       * region
+       * console.log(res[0].xAxis.region)
+       *
+       * length
+       * console.log(res.length);
+       */
 
       this.bar.xAxis.data = Object.values(res[0].xAxis.data);
       this.bar.series[0].data = res[0].series[0].data;
--- src/resources/assets/js/components/views/bantenprov/dd-sekolah/DashboardDDSekolah.vue
+++ src/resources/assets/js/components/views/bantenprov/dd-sekolah/DashboardDDSekolah.vue
@@ -2,42 +2,17 @@
   <div>
 
     <div class="row">
-      <div class="col-md-4 mb-3">
-        <div class="card card-media text-white bg-primary">
+      <div class="col-lg-6 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #2196F3;">
           <div class="card-body">
-            <div class="media">
-              <div class="media-body text-truncate">
-                <div class="card-body">
-                  <dd-sekolah-bar-03></dd-sekolah-bar-03>
-                </div><!-- /.card-body -->
-              </div>
-            </div>
+            <dd-sekolah-bar-03></dd-sekolah-bar-03>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
       </div><!-- /col -->
-      <div class="col-md-4 mb-3">
-        <div class="card card-media text-white bg-success">
+      <div class="col-lg-6 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #4CAF50;">
           <div class="card-body">
-            <div class="media">
-              <div class="media-body text-truncate">
-                <div class="card-body">
-                  <dd-sekolah-pie-03></dd-sekolah-pie-03>
-                </div><!-- /.card-body -->
-              </div>
-            </div>
-          </div><!-- /.card-body -->
-        </div><!-- /.card -->
-      </div><!-- /col -->
-      <div class="col-md-4 mb-3">
-        <div class="card card-media text-white bg-warning">
-          <div class="card-body">
-            <div class="media">
-              <i class="fa fa-calendar fa-card-media-avatar mr-3" aria-hidden="true"></i>
-              <div class="media-body text-truncate">
-                <h5 class="mt-0">Agenda</h5>
-                <span><i class="fa fa-dot-circle-o" aria-hidden="true"></i> 30</span>
-              </div>
-            </div>
+            <dd-sekolah-pie-03></dd-sekolah-pie-03>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
       </div><!-- /col -->
@@ -46,10 +21,8 @@
     <!-- ===================================================================== -->
 
     <div class="row">
-
-      <div class="col-md-8 mb-3">
-
-        <div class="card h-100">
+      <div class="col-lg-8 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #009688;">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
             Dapodik Data Sekolah Berdasarkan Wilayah
@@ -58,13 +31,9 @@
             <dd-sekolah-bar-01></dd-sekolah-bar-01>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
-
       </div><!-- /col -->
-
-
-      <div class="col-md-4 mb-3">
-
-        <div class="card h-100">
+      <div class="col-lg-4 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #FF9800;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
             Dapodik Data Sekolah Berdasarkan Wilayah
@@ -73,14 +42,14 @@
             <dd-sekolah-pie-01></dd-sekolah-pie-01>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
-
       </div><!-- /col -->
-      <div class="row">
+    </div><!-- /.row -->
 
-      </div>
-      <div class="col-md-4 mb-3">
+    <!-- ===================================================================== -->
 
-        <div class="card h-100">
+    <div class="row">
+      <div class="col-lg-6 mb-3">
+        <div class="card h-100" style="color: #fff; background-color: #F44336;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
             Dapodik Data Sekolah Berdasarkan Tahun
@@ -90,10 +59,8 @@
           </div><!-- /.card-body -->
         </div><!-- /.card -->
       </div><!-- /.col -->
-
-      <div class="col-md-8 mb-3">
-
-        <div class="card h-100">
+      <div class="col-lg-6 mb-3">
+        <div class="card text-white bg-dark h-100">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
             Dapodik Data Sekolah Berdasarkan Tahun
@@ -102,11 +69,8 @@
             <dd-sekolah-bar-02></dd-sekolah-bar-02>
           </div><!-- /.card-body -->
         </div><!-- /.card -->
-
-      </div>
-
+      </div><!-- /col -->
     </div><!-- /.row -->
 
   </div>
 </template>
-
